da0be5ded67b23be8a42fd7069dfaded6eb280fa
jaymeeu <rasaqadewuyi@gmail.com>
2024-03-17 17:11:45 +0100

    need revert cloudinary

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="image_transformation"
       options={{
         multiple: false,
         resourceType: "image",
